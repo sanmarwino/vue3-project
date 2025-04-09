c44764f7730fbd207399b0c4ee54fb2c64de1c66
sanmarwino <marwinmedado@gmail.com>
2025-04-09 22:20:07 +0800

    update code

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,13 @@
 <script setup>
 import { defineAsyncComponent } from 'vue';
 
-const Header = defineAsyncComponent(() =>import('./components/Header.vue'))
-const HeroSection = defineAsyncComponent(() =>import('./components/HeroSection.vue'))
-const AboutSection = defineAsyncComponent(() =>import('./components/AboutSection.vue'))
-const ExperienceAndSkill = defineAsyncComponent(() =>import('./components/ExperienceAndSkill.vue'))
-const SeminarSection = defineAsyncComponent(() =>import('./components/SeminarSection.vue'))
-const ProjectSection = defineAsyncComponent(() =>import('./components/ProjectSection.vue'))
-const ContactSection = defineAsyncComponent(() =>import('./components/ContactSection.vue'))
-const Footer = defineAsyncComponent(() =>import('./components/Footer.vue'))
-const BackToTop = defineAsyncComponent(() =>import('./components/BackToTop.vue'))
+const Header = defineAsyncComponent(() =>import('@/components/Header.vue'))
+const HeroSection = defineAsyncComponent(() =>import('@/components/HeroSection.vue'))
+const AboutSection = defineAsyncComponent(() =>import('@/components/AboutSection.vue'))
+const ExperienceAndSkill = defineAsyncComponent(() =>import('@/components/ExperienceAndSkill.vue'))
+const SeminarSection = defineAsyncComponent(() =>import('@/components/SeminarSection.vue'))
+const ProjectSection = defineAsyncComponent(() =>import('@/components/ProjectSection.vue'))
+const ContactSection = defineAsyncComponent(() =>import('@/components/ContactSection.vue'))
+const Footer = defineAsyncComponent(() =>import('@/components/Footer.vue'))
+const BackToTop = defineAsyncComponent(() =>import('@/components/BackToTop.vue'))
 </script>
